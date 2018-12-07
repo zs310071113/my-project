d35596eebb9e5d8f9907657563fa6b7574f59f4e
zs <310071113@qq.com>
2018-12-07 13:23:17 +0800

    第一次上传vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+		<Button>Default</Button>
+		<Button type="primary">Primary</Button>
+		<Button type="dashed">Dashed</Button>
+		<Button type="text">Text</Button>
+		<br><br>
+		<Button type="info">Info</Button>
+		<Button type="success">Success</Button>
+		<Button type="warning">Warning</Button>
+		
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,24 @@
+<template>
+	<Button type="default" ghost>Default</Button>
+    <Button type="primary" ghost>Primary</Button>
+    <Button type="dashed" ghost>Dashed</Button>
+    <Button type="text" ghost>Text</Button>
+    <Button type="info" ghost>Info</Button>
+    <Button type="success" ghost>Success</Button>
+    <Button type="warning" ghost>Warning</Button>
+    <Button type="error" ghost>Error</Button>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
